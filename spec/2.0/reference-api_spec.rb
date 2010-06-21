e18f61ebec389cc1fa9298baef1007b61f485e37
Cyril Rohr <cyril.rohr@irisa.fr>
2010-06-21 09:07:38 +0200

    More tests for version /2.0 of reference-api.

--- spec/2.0/reference-api_spec.rb
+++ spec/2.0/reference-api_spec.rb
@@ -20,4 +20,25 @@ describe "Reference API 2.0" do
       result['uid'].should == 'rennes'
     end
   end
+  
+  it "should correctly fetch a specific version of a node, using a timestamp" do
+    timestamp = 1266735757
+    get uri_for("/2.0/grid5000/sites/rennes/clusters/paradent/nodes/paradent-1?version=#{timestamp}"), :accept => :json do |response|
+      response.code.should == 200
+      version_uri = parse(response)['links'].find{|link| 
+        link['title'] == 'version'}['href']
+      get uri_for(version_uri) do |response|
+        response.code.should == 200
+        Time.parse(parse(response)['date']).to_i.should <= timestamp
+      end
+    end
+  end
+  
+  it "should correctly fetch a specific version of a cluster, using a given commit hash" do
+    hash = "a7f8aa62fc101dd41b6febbc7b1a9ce4ccb95e1a"
+    get uri_for("/2.0/grid5000/sites/rennes/clusters/paradent?version=#{hash}"), :accept => :json do |response|
+      response.code.should == 200
+      parse(response)['version'].should == hash
+    end
+  end
 end
